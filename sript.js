f0b323c465a70579ac61e01c0f4e8aaafce23517
Richa20-web <rk3833@srmist.edu.in>
2025-06-27 12:58:16 +0530

    sript.js

---
+++ sript.js
@@ -0,0 +1,43 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.add("show-mobile-menu");
+});
+
+closeMenuBtn.addEventListener("click", () => {
+  header.classList.remove("show-mobile-menu");
+});
+
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
